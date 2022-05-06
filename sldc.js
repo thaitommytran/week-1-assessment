3c258122c57be3ce5282152bde19d90c5bc4f7bb
Thai Tran <thai.tommy.tran@gmail.com>
2022-05-06 09:10:23 -0500

    software development life cycle

--- sldc.js
+++ sldc.js
@@ -9,8 +9,8 @@
 // Analysis of Requirements
 // Design
 // Implementation
-// Maintain
 // Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
@@ -18,4 +18,9 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added.";
+let analysisOfRequirements = 'This step is used to define and document the product requirements of the customer';
+let design = 'Turning software specifications into a design plan - reviewing product robustness, budget, time, and risk assessments to achieve the best end product.';
+let implementation = 'The Software design is translated into source code. All the components of the software are implemented in this phase.';
+let testingAndIntegration = 'In this phase, software is tested/debugged. It is retested until the software meets customer specifications.';
+let maintain = 'Maintaining a software after deployment to resolve any issues that arise in the future.';
