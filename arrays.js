d475ab94b855c5e9f83c93846b48a96b3365d6e5
Thai Tran <thai.tommy.tran@gmail.com>
2022-05-06 09:24:27 -0500

    arrays

--- arrays.js
+++ arrays.js
@@ -4,11 +4,13 @@
 const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
+let colorCopy = faveColors.slice();
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+colorCopy.push('white');
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -19,7 +21,7 @@ const faveColors = ['red', 'green', 'black']
 const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
-
+let middleNums = numbers.slice(1, 4);
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -34,3 +36,12 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+let answers = [];
+
+for (let i = 0; i < bigOrSmallArray.length; i++) {
+  if (bigOrSmallArray[i] > 100) {
+    answers.push('big');
+  } else {
+    answers.push('small');
+  }
+}
